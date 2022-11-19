2e71179301173265ab305c2cbc4af2b2c0bb2bf8
Master Chit Chit <aztawong@aliyun.com>
2022-11-20 01:42:38 +0800

    Save image to i-Formula

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Response, Flask, request
 from threading import Thread, Lock
 #from classes.CSICamera import CSICamera
 from classes.IFormula import IFormula
+from classes.DataIntelligent import DataIntelligent
 
 import time
 
@@ -33,6 +34,7 @@ class CSICamera:
         self.capture_width = capture_width
         self.capture_height = capture_height
         self.framerate = framerate
+
 
     def init_camera(self):
         import cv2
@@ -66,6 +68,7 @@ class CSICamera:
                 video_frame = frame.copy()
 
             self.frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    
 
     def run(self):
         self.poll_camera()
@@ -80,7 +83,7 @@ class CSICamera:
         print('stopping CSICamera')
         time.sleep(.5)
         del self.camera
-
+    
 
 
 
@@ -90,9 +93,12 @@ video_frame = None
 global thread_lock
 thread_lock = Lock()
 
+data = DataIntelligent()
 
 def encodeFrame():
     global thread_lock
+    #data = DataIntelligent()
+
     import cv2
     while True:
         # Acquire thread_lock to access the global video_frame object
@@ -101,8 +107,32 @@ def encodeFrame():
             if video_frame is None:
                 continue
             return_key, encoded_image = cv2.imencode(".jpg", video_frame)
+            
             if not return_key:
                 continue
+            else:
+                if data.getSnapshot():
+                    try:
+                        if data.getPhotocat()=='1':  #free
+                            cv2.imwrite(data.saveFree(), video_frame)
+
+                        elif data.getPhotocat()=='2': #left
+                            cv2.imwrite(data.saveLeft(), video_frame)
+
+                        elif data.getPhotocat()=='3': #right
+                            cv2.imwrite(data.saveRight(), video_frame)
+
+                        elif data.getPhotocat()=='4': #block
+                            cv2.imwrite(data.saveBlocked(), video_frame)
+
+                        else:
+                            data.setStatus('invald code, no snapshot taken')
+                            print('invald code, no snapshot taken')
+                    except:
+                        print('error')
+                    data.resetStatus()
+                    
+                    
 
         # Output image as a byte array
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
@@ -115,7 +145,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def name():
-    return "i-Formula - Noob One"
+    return 'i-Formula - Noob One'
 
 @app.route("/camlive")
 def streamFrames():
@@ -123,16 +153,17 @@ def streamFrames():
 
 @app.route('/controls')
 def controls():
+    action = ''
     i = request.args.get('c')
     if i== '1':
         mini_i_formula.forward()
-        print('start')
+        action = 'start'
     elif i=='2':
         mini_i_formula.left()
-        print('left')
+        action = 'left'
     elif i=='3':
         mini_i_formula.right()
-        print('right')
+        action = 'right'
     elif i=='4':
         mini_i_formula.backward()
         action = 'backward'
@@ -142,10 +173,23 @@ def controls():
 
     return action
 
+@app.route('/speed')
+def speedchanged():
+    s = request.args.get('c')
+    mini_i_formula.speedchanged(s)
+    return f'Speed Changed to {s}'
+
+@app.route('/takesnap')
+def sample():
+    data.resetStatus()
+    i = request.args.get('i')
+    data.snap(i)
+    return data.snapstatus()
 
 if __name__ == '__main__':
     cam = CSICamera(image_w=720, image_h=480, capture_width=1080, capture_height=720)
-    mini_i_formula = IFormula(0.5)
+    mini_i_formula = IFormula(0.1)
+    
     
     # Create a thread and attach the method that captures the image frames, to it
     process_thread = Thread(target=cam.update)
@@ -154,4 +198,7 @@ if __name__ == '__main__':
     # Start the thread
     process_thread.start()
 
+    # start the Flask Web Application
+    # While it can be run on any feasible IP, IP = 0.0.0.0 renders the web app on
+    # the host machine's localhost and is discoverable by other machines on the same network
     app.run("0.0.0.0", port="12321")
