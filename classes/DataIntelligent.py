abcd2d04939e5d8596e52795c439cebd570742d9
Master Chit Chit <aztawong@aliyun.com>
2022-11-20 01:44:31 +0800

    AI Training Class

---
+++ classes/DataIntelligent.py
@@ -0,0 +1,93 @@
+import os
+from uuid import uuid1
+
+class DataIntelligent:
+    
+    def __init__(self):
+        self.blocked_dir = 'i/blocked'
+        self.free_dir = 'i/free'
+        self.left_dir = 'i/left'
+        self.right_dir = 'i/right'
+        print(os.getcwd())
+        try:
+            os.makedirs(self.free_dir)
+            os.makedirs(self.blocked_dir)
+            os.makedirs(self.left_dir)
+            os.makedirs(self.right_dir)
+            print('All folders created successfully.')
+        except OSError as error:
+            print(f'Directories not created because ${error}')
+        self.photocat = -1
+        self.snapshot = False
+        self.ssstatus = ''
+    
+    def __save_snapshot(self, directory, image):
+        image_path = os.path.join(directory, f'{str(uuid1())}.jpg')
+        with open(image_path, 'wb') as f:
+            f.write(image.value)
+    
+    def saveFree(self):
+        return os.path.join(self.free_dir, f'{str(uuid1())}.jpg' )
+        #try: 
+        #    self.__save_snapshot(self.free_dir, image)
+        #    return 'Save successfully'
+        #except:
+        #    return 'Save failed'
+            
+    
+    def saveBlocked(self):
+        return os.path.join(self.blocked_dir, f'{str(uuid1())}.jpg' )
+        #try:
+        #    self.__save_snapshot(self.blocked_dir, image)
+        #    return 'Save successfully'
+        #except:
+        #    return 'Save failed'
+    
+    def saveLeft(self):
+        return os.path.join(self.left_dir, f'{str(uuid1())}.jpg' )
+
+        #try:
+        #    self.__save_snapshot(self.left_dir, image)
+        #    return 'Save successfully'
+        #except:
+        #    return 'Save failed'
+    
+    def saveRight(self):
+        return os.path.join(self.right_dir, f'{str(uuid1())}.jpg' )
+
+        #try:
+        #    self.__save_snapshot(self.right_dir, image)
+        #    return 'Save successfully'
+        #except:
+        #    return 'Save failed'
+        
+    def count(self):
+        free = len(os.listdir(self.free_dir))
+        blocked = len(os.listdir(self.blocked_dir))
+        left = len(os.listdir(self.left_dir))
+        right = len(os.listdir(self.right_dir))
+        
+        jsonTxt = f"\{'free': {free}, 'blocked': {block}, 'left': {left}, 'right': {right} \}"
+        
+        return jsonTxt
+    
+    def getSnapshot(self):
+        return self.snapshot
+    
+    def getPhotocat(self):
+        return self.photocat
+    
+    def resetStatus(self):
+        self.snapshot = False
+        self.photocat = -1
+    
+    def snap(self, cat):
+        #self.data = DataIntelligent()
+        self.snapshot = True
+        self.photocat = cat
+    
+    def setStatus(status):
+        self.ssstatus = status
+        
+    def snapstatus(self):
+        return self.ssstatus
